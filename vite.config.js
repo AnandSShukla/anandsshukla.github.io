211f34465b393760ac5211917f25b9165f290332
Anand Shukla <anandshhukla6812@gmail.com>
2026-06-06 20:58:49 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // ✏️  Change '/crypto-vault/' to match your IIS application / virtual directory name.
 //     Must start AND end with a slash.  e.g.  '/tools/crypto-vault/'
-const BASE_PATH = '/crypto-vault/'
+// const BASE_PATH = '/crypto-vault/'
 
 export default defineConfig({
   plugins: [react()],
-  base: BASE_PATH,
+  base: "/",
 })
